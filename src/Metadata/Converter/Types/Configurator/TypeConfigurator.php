b965faf4be30236094c0995adddc302f56895cd0
Toon Verwerft <toonverwerft@gmail.com>
2023-01-27 09:09:46 +0100

    Improve xsd parsing

--- src/Metadata/Converter/Types/Configurator/TypeConfigurator.php
+++ src/Metadata/Converter/Types/Configurator/TypeConfigurator.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Soap\WsdlReader\Metadata\Converter\Types\Configurator;
 
+use GoetasWebservices\XML\XSDReader\Schema\Inheritance\Restriction;
 use GoetasWebservices\XML\XSDReader\Schema\Type\Type as XsdType;
 use Soap\Engine\Metadata\Model\XsdType as MetaType;
 use Soap\WsdlReader\Metadata\Converter\Types\TypesConverterContext;
@@ -11,19 +12,18 @@ final class TypeConfigurator
 {
     public function __invoke(MetaType $metaType, XsdType $xsdType, TypesConverterContext $context): MetaType
     {
-        $restrictions = $xsdType->getRestriction();
         $parent = $xsdType->getParent();
-        $extension = $xsdType->getExtension();
-        $base = $restrictions?->getBase() ?: $extension?->getBase();
+        $base = $parent?->getBase();
+        $checks = $parent instanceof Restriction ? $parent->getChecks() : [];
 
         return $metaType
             ->withMeta([
                 ...$metaType->getMeta(),
                 'docs' => $xsdType->getDoc(),
                 'abstract' => $xsdType->isAbstract(),
-                'restrictions' => $restrictions ? $restrictions->getChecks() : [],
-                'parent' => $parent && method_exists($parent, 'getChecks') ? $parent->getChecks() : [],
+                'restrictions' => $checks,
                 'extends' => $base?->getName() ?? '',
+                'extendsNamespace' => $base?->getSchema()->getTargetNamespace() ?? '',
             ])
             ->withBaseType($base?->getName() ?? '');
     }
